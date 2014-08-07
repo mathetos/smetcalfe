6a797a7c1eaccd96a46b92c1f28f9f7636343c3c
mathetos <reachme@mattcromwell.com>
2014-08-06 23:01:43 -0700

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Editor
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<span class="powered-by">All Rights Reserved &copy; Stephen Metcalfe</span>
+			<span class="sep"> | </span>
+			<span class="webdev">Developed by <a href="http://mattcromwell.com" target="_blank">mattcromwell.com</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
